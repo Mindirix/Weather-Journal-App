682e41fc03002e3baf334a3127c30e212b37aaca
Ahmed Moussa <ahmed_moussa@tutanota.com>
2022-02-12 19:30:16 +0200

    add server code

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
+const app = express();
 
+/* Dependencies */
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+//cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, () => {
+    console.log(`runnning on local host: ${port}`);
+  });
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,15 @@
 /* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '&units=metric&appid=96caabaa384249b2f564e0b4fba24dfb';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+document.getElementById('generate').addEventListener('click', ()=> {
+    console.log("generate"); //test
+    
+})
+
+// TODO - clear all tests
